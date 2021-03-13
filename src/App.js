803b662eb517f0d34d9a51afdfafae838f731051
mahavinay <mahavinay28@gmail.com>
2021-03-13 22:57:28 +0100

    change in indentation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import RestCountriesHome from './components/RestCountriesHome'
-import Navigation from './components/Navigation'
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RestCountriesHome from "./components/RestCountriesHome";
+import Navigation from "./components/Navigation";
 
 function App() {
-  
-
- 
-
-  return (
-    <div className="App">
-       <Navigation/>
-       <RestCountriesHome/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navigation />
+            <RestCountriesHome />
+        </div>
+    );
 }
 
 export default App;
